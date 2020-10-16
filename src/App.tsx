2a58e839cc8d95a6805d835933aed4497bc1a359
diego.harrison <diegoharrisonsm@gmail.com>
2020-10-16 00:23:12 -0300

    estructure of application web with react

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './styles/global.css';
+import './styles/pages/landing.css'
+
+function Title() {
+  return (
+    <h1></h1>    
+  )      
+}
+
+function App() {
+  return (
+    <div id="page-landing">      
+      <div className="content wrapper">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
